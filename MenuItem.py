989bea0294ba8e8d9e7d0a7ebc2283818198175a
Sofia Rytik <dv11@3soft.ru>
2021-12-03 15:32:14 +0300

    changed back button

--- MenuItem.py
+++ MenuItem.py
@@ -17,6 +17,7 @@ class Menu(MenuItem):
         super().__init__(title)
         self.__items = []
         self.__running = True
+        self.parent_item = None
 
     def set_items(self, items: [MenuItem]):
         self.__items = items
@@ -31,7 +32,13 @@ class Menu(MenuItem):
             self.handle_user_input()
 
     def add_menu_item(self, item: MenuItem):
+        item.parent_item = self
         self.__items.append(item)
+
+    def add_back_button(self):
+        back_button = Menu("Back")
+        back_button.set_items(self.parent_item.get_items())
+        self.__items.append(back_button)
 
     def print_menu(self):
         i = 0
@@ -69,16 +76,12 @@ if __name__ == "__main__":
     first_menu = Menu("First menu")
     second_menu = Menu("Second menu")
     simple_item1 = SimpleMenuItem("simple item 1", simple_func)
-    back_button_first_menu = Menu("Back")
-    back_button_first_menu.set_items(main_menu.get_items())
-    back_button_second_menu = Menu("Back")
-    back_button_second_menu.set_items(main_menu.get_items())
 
     main_menu.add_menu_item(first_menu)
     main_menu.add_menu_item(second_menu)
     first_menu.add_menu_item(simple_item1)
-    first_menu.add_menu_item(back_button_first_menu)
+    first_menu.add_back_button()
     second_menu.add_menu_item(simple_item1)
-    second_menu.add_menu_item(back_button_second_menu)
+    second_menu.add_back_button()
 
     main_menu.run()
